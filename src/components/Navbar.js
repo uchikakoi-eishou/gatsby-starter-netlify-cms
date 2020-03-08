538b9b66fa78a8692dd39e2eb1f3767c23c6dd25
uchikakoi-eishou <kakoi.0501souta@gmail.com>
2020-03-08 16:43:33 +0900

    navbarの変更

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,11 +60,11 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
               <Link className="navbar-item" to="/about">
-                About
+                Ei-Showについて
+              </Link>
+              <Link className="navbar-item" to="/blog">
+                ブログ
               </Link>
               <Link className="navbar-item" to="/products">
                 Products
